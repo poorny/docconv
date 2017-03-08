1565e08e71f3d6ee1ba0fb657af807b65966d6b5
Avelino <thiago@avelino.xxx>
2017-03-08 16:37:23 -0300

    try return pdfinfo 0.18.*

--- pdf_test.go
+++ pdf_test.go
@@ -38,7 +38,29 @@ func TestConvertPDF(t *testing.T) {
 		"JavaScript":     "no",
 		"Pages":          "1",
 	}
-	if !reflect.DeepEqual(meta, want) {
-		t.Errorf("ConvertPDF() meta = %#v; not %#v", meta, want)
+
+	want_v018 := map[string]string{
+		"Creator":      "Acrobat PDFMaker 7.0.7 for Word",
+		"Producer":     "Acrobat Distiller 7.0.5 (Windows)",
+		"CreationDate": "Wed Jun  4 15:44:00 2008",
+		"Page size":    "612 x 792 pts (letter)",
+		"File size":    "20597 bytes",
+		"PDF version":  "1.6", "Title": "PDF Test Page",
+		"Author":    "Yukon Department of Education",
+		"ModDate":   "Wed Jun  4 15:47:36 2008",
+		"Tagged":    "yes",
+		"BleedBox":  "0.00     0.00   612.00   792.00",
+		"Encrypted": "yes (print:yes copy:no change:no addNotes:no)",
+		"MediaBox":  "0.00     0.00   612.00   792.00",
+		"Pages":     "1",
+		"CropBox":   "0.00     0.00   612.00   792.00",
+		"TrimBox":   "0.00     0.00   612.00   792.00",
+		"ArtBox":    "0.00     0.00   612.00   792.00",
+		"Optimized": "yes",
+	}
+	if !reflect.DeepEqual(meta, want_v018) {
+		if !reflect.DeepEqual(meta, want) {
+			t.Errorf("ConvertPDF() meta = %#v; not %#v", meta, want)
+		}
 	}
 }
