ac6e742baab78731f050565cba034d6d6b810ca2
Joel M. de Souza <contato@joelmdesouza.com>
2019-03-26 15:06:35 -0300

    Add case for test in travis (#17)
    * Verify pdftotext version
    
    * Change dist to test pdftotext version
    
    * Add wantTravis to TestConvertPDF pass on Travis
    
    * Remove blank lines

--- pdf_test.go
+++ pdf_test.go
@@ -38,7 +38,6 @@ func TestConvertPDF(t *testing.T) {
 		"JavaScript":     "no",
 		"Pages":          "1",
 	}
-
 	want_v018 := map[string]string{
 		"Creator":      "Acrobat PDFMaker 7.0.7 for Word",
 		"Producer":     "Acrobat Distiller 7.0.5 (Windows)",
@@ -58,9 +57,36 @@ func TestConvertPDF(t *testing.T) {
 		"ArtBox":    "0.00     0.00   612.00   792.00",
 		"Optimized": "yes",
 	}
+	wantTravis := map[string]string{
+		"Author":         "Yukon Department of Education",
+		"Encrypted":      "yes (print:yes copy:no change:no addNotes:no algorithm:RC4)",
+		"ArtBox":         "0.00     0.00   612.00   792.00",
+		"File size":      "20597 bytes",
+		"PDF version":    "1.6",
+		"Creator":        "Acrobat PDFMaker 7.0.7 for Word",
+		"ModDate":        "Wed Jun  4 15:47:36 2008",
+		"Tagged":         "yes",
+		"Suspects":       "no",
+		"CropBox":        "0.00     0.00   612.00   792.00",
+		"Optimized":      "yes",
+		"CreationDate":   "Wed Jun  4 15:44:00 2008",
+		"Form":           "none",
+		"Page size":      "612 x 792 pts (letter)",
+		"Page rot":       "0",
+		"TrimBox":        "0.00     0.00   612.00   792.00",
+		"MediaBox":       "0.00     0.00   612.00   792.00",
+		"BleedBox":       "0.00     0.00   612.00   792.00",
+		"Title":          "PDF Test Page",
+		"Producer":       "Acrobat Distiller 7.0.5 (Windows)",
+		"UserProperties": "no",
+		"JavaScript":     "no",
+		"Pages":          "1",
+	}
 	if !reflect.DeepEqual(meta, want_v018) {
-		if !reflect.DeepEqual(meta, want) {
-			t.Errorf("ConvertPDF() meta = %#v; not %#v", meta, want)
+		if !reflect.DeepEqual(meta, wantTravis) {
+			if !reflect.DeepEqual(meta, want) {
+				t.Errorf("ConvertPDF() meta = %#v; not %#v", meta, want)
+			}
 		}
 	}
 }
